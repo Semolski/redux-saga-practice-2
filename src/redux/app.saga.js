62a269a176ebed572dd9e548ba38de200114b2f9
Sarah Mayoral-Osmolski <sarah@semolski.com>
2021-11-03 13:34:39 -0500

    Working with take(), takeEvery(), and takeLatest()
    functions in redux saga

---
+++ src/redux/app.saga.js
@@ -0,0 +1,19 @@
+import { takeLatest, takeEvery,delay, put } from 'redux-saga/effects';
+
+export function* onIncrement() {
+  yield console.log('I am incremented');
+  // yield delay(5000);
+  yield put({ type: 'INCREMENT_FROM_SAGA' });
+}
+
+// export function* incrementSaga() {
+//   yield takeLatest('INCREMENT', onIncrement);
+// }
+
+export function* incrementSaga() {
+  // while(true){
+  //   yield take('INCREMENT');
+  //   yield console.log('I am incremented');
+  // }
+  yield takeEvery('INCREMENT', onIncrement)
+}
